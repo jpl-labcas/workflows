a61976bafe52b64a657110808746670fa6774f0c
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2025-02-06 18:44:03 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# LabCas Workflow
+
+Run workflows for Labcas
+
+
+## install
+
+    pip install -e '.[dev]'
+
+
+## Run
+
+Set you local AWS credentials to access the data (see 
+
+
+    ./aws-login.darwin.amd64
+
---
+++ src/labcas/workflow/manager/main.py
@@ -0,0 +1,34 @@
+from labcas.workflow.manager import DataStore
+from labcas.workflow.steps.alphan.process import process_img
+
+# in another terminal
+# from dask.distributed import Client
+# dask_client = Client(processes=False, threads_per_worker=4,
+#            n_workers=1, memory_limit='2GB')
+# dask_client
+
+
+def process_collection(bucket_name, in_prefix, out_prefix, fun, kwargs):
+    # Use a breakpoint in the code line below to debug your script.
+
+    datastore = DataStore(bucket_name, in_prefix, out_prefix)
+
+    for obj in datastore.get_inputs():
+        in_key = obj['Key']
+        print(in_key)
+        fun(
+            datastore,
+            in_key,
+            **kwargs
+        )
+
+
+if __name__ == '__main__':
+    process_collection(
+        'edrn-bucket',
+        'nebraska_images',
+        'nebraska_images_nuclei',
+        process_img,
+        dict(tile_size=64)
+    )
+
